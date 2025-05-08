9df28607a1843ea4cc07e087e919d276f3a76326
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-05-08 11:51:30 +0100

    Release v0.1.1

--- release.bash
+++ release.bash
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 # Some files have mildly sensitive information,
-# like a `curl v2.wttr.in` shortcut that reveals where I live
+# like a `curl v2.wttr.in` shortcut that reveals where I live.
+# Run this script, and then run
+# rsync -av --exclude='.git' --exclude='.jj' ~/dotfiles/ ~/projects/public_dotfiles/
+# to deploy to the public repo
 
 # Set public = true in home.nix
 sed -i 's/public ? false/public ? true/' home.nix
